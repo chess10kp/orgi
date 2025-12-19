e6843016fbaba3647c54d3c7394a50a25fb8c5a4
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-12-19 12:37:33 -0500

    fix: update install script

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ set -e
 curl -LO https://github.com/chess10kp/orgi/releases/download/v0.1.0/orgi-linux-x64.tar.gz
 
 tar -xzf orgi-linux-x64.tar.gz
-cd orgi-linux-x64
 
-sudo cp orgi /usr/local/bin/
+sudo cp orgi-linux-x64/Orgi.Core /usr/local/bin/orgi
 sudo chmod +x /usr/local/bin/orgi
