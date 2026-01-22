2326dd3144e135e18d85c0afe8ce45f009e9bb21
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2026-01-21 20:13:36 -0500

    Add issue

--- gh-release.sh
+++
@@ -1,4 +0,0 @@
-gh release create v0.1.8 \
-   orgi-linux-x64.tar.gz \
-   --title "v0.1.8" \
-   --notes "Improved add command to not prompt for priority, state, and tags when title provided as argument"
